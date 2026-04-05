7a4c8db2f8cab53bee24e13cde1bedef0a5b7d07
robin <robin@Sandhyas-MacBook-Air.local>
2026-04-06 00:07:14 +0530

    add check for sen notification if user valid or not via redis as message queue

--- src/main/java/com/notifications/notification_service/service/NotificationService.java
+++ src/main/java/com/notifications/notification_service/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.notifications.notification_service.service;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +18,14 @@ import com.notifications.notification_service.kafka.NotificationProducer;
 public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final NotificationProducer notificationProducer;
+    private final RedisTemplate<String,String> redisTemplate;
 
 
 
-    public NotificationService(NotificationRepository notificationRepository,NotificationProducer notificationProducer) {
+    public NotificationService(NotificationRepository notificationRepository,NotificationProducer notificationProducer,RedisTemplate<String,String> redisTemplate) {
         this.notificationRepository = notificationRepository;
         this.notificationProducer = notificationProducer;
+        this.redisTemplate = redisTemplate;
     }
 
     public String createNotification(NotificationDto notificationDto) {
@@ -33,6 +36,16 @@ public class NotificationService {
         // }catch(InterruptedException e){
         //     Thread.currentThread().interrupt();
         // } // Simulate processing time 
+       String UserId=notificationDto.getUserId();
+       String userData=redisTemplate.opsForValue().get("user:"+UserId);
+       if(userData==null){
+        log.warn("User data not found in Redis for userId: " + UserId);
+        return "User data not found in Redis for userId: " + UserId;
+       }
+        log.info("User data retrieved from Redis for userId: " + UserId);
+
+
+
 
         Notification notification = Notification.builder()
                 .userId(notificationDto.getUserId())
